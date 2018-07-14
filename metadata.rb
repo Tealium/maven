b5fb503d8aa145fdaa780e86a2f7a77eefe13002
Aaron East <aaron.east@tealium.com>
2018-07-13 17:55:47 -0700

    bumped cb and changed depends from java to java_se

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email "bryan.berry@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures maven"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.5"
+version          "0.3.6"
 
-depends "java"
+depends "java_se"
 depends "ark"
 
 %w{ debian ubuntu centos redhat fedora }.each do |os|
